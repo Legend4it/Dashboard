47d22b83512a8a67eb640f648fd3692740d4cdea
Legend4itm <abdulhussein.ali@gmail.com>
2021-04-11 12:02:31 +0200

    Change Project name, add LoginFormComponent

--- login-form/src/Components/App.js
+++ login-form/src/Components/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LoginFormComponent from './LoginFormComponent.js'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <LoginFormComponent />
     </div>
   );
 }
---
+++ login-form/src/Components/LoginFormComponent.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function LoginFormComponent(){
+    return(
+        <div>
+            <input type="text" placeholder="User Name"/>
+            <input type="password" placeholder="Pass Word"/>
+        </div>
+    );
+}
+
+export default LoginFormComponent;
